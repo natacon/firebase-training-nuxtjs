3b3da0f1aec221cf9e9b9e3a182b3196905948ea
natacon <uikun0113@gmail.com>
2020-04-14 00:25:59 +0900

    ログイン後の画面を仮で作成

---
+++ pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class Home extends Vue {
+
+}
+
+</script>
